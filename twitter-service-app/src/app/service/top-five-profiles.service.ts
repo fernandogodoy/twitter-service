7f83aacbaa9d02194618d680600974a388d704cf
Fernando Godoy <fernandogodoy18@gmail.com>
2019-03-30 21:00:19 -0300

    correção profiles

--- twitter-service-app/src/app/service/top-five-profiles.service.ts
+++ twitter-service-app/src/app/service/top-five-profiles.service.ts
@@ -10,6 +10,6 @@ export class TopFiveProfilesService {
   constructor(private http: HttpClient) { }
 
   getTopFiveProfiles() :  Observable<any> {
-    return this.http.get("https://twitter-filter-service.herokuapp.com/user-followers-count/top-5");
+    return this.http.get("https://twitter-filter-service.herokuapp.com/twitter-profile/user-followers-count/top-5");
   }
 }
